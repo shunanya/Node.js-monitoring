011d2014a155bf44175a9112a21ba6fcfc80ecec
shunanya <simon.hunanyan@gmail.com>
2014-11-07 15:09:55 +0400

    Version update

--- node_monitor_plugin/src/monitor.js
+++ node_monitor_plugin/src/monitor.js
@@ -1,6 +1,6 @@
 /*
  * name: node-monitor
- * version: 0.3.9
+ * version: 0.3.10
  * description: Node.js server monitor module
  * repository: git://github.com/shunanya/Node.js-monitoring.git
  * dependencies: 
